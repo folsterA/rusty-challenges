4a25e0d03aeb0f97bea64c083ef89206ac2bff21
virtual_chaotic <virtualchaotic@gmail.com>
2025-07-03 10:59:33 -0700

    updated challenge #65

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rusty-challenges
 A collection of challenges for me to do in Rust.
+
+## Challenge #65: Text To Hex Converter
+
+This challenge is to create a program that converts text input into human-readable, hexidecimal output.
--- txt_hex_65/src/main.rs
+++ txt_hex_65/src/main.rs
@@ -1,5 +1,51 @@
 use std::io;
 
+fn text_to_hex(text: &str) -> String {
+    // transform into bytes
+    let bytes = &text.as_bytes();
+
+    // build bytes as hex string
+    let mut x: Vec<String> = Vec::new();
+    for byte in bytes.iter()
+    {
+        let s = format!("{:#x}", byte);
+        x.push(s);
+    }
+    x.join(" ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn text()
+    {
+        let text = "hello";
+        let hex_truth = "0x68 0x65 0x6c 0x6c 0x6f";
+        let hex = &text_to_hex(text);
+        assert_eq!(hex, hex_truth);
+    }
+
+    #[test]
+    fn hex()
+    {
+        let text = "1234567890";
+        let hex_truth = "0x31 0x32 0x33 0x34 0x35 0x36 0x37 0x38 0x39 0x30";
+        let hex = &text_to_hex(text);
+        assert_eq!(hex, hex_truth);
+    }
+
+    #[test]
+    fn whitespace()
+    {
+        let text = "w h i \t e \n s_p a c e";
+        let hex_truth = "0x77 0x20 0x68 0x20 0x69 0x20 0x9 0x20 0x65 0x20 0xa 0x20 0x73 0x5f 0x70 0x20 0x61 0x20 0x63 0x20 0x65";
+        let hex = &text_to_hex(text);
+        assert_eq!(hex, hex_truth);
+    }
+}
+
 fn main() {
     // create buffer
     let mut buffer = String::new();
@@ -7,19 +53,12 @@ fn main() {
     // read into buffer
     let stdin = io::stdin();
     let _bytes_read = stdin.read_line(&mut buffer).unwrap();
-    buffer.pop(); // remove '\n' from the end.
-
-    // transform into bytes
-    let bytes = &buffer.clone().into_bytes();
-
+    buffer.pop(); // remove 'CR' from the end.
+    buffer.pop(); // remove 'LF' from the end.
+    
     // build bytes as hex string
-    let mut hex = String::new();
-    for byte in bytes.iter()
-    {
-        let f = format!("{:#x} ", byte);
-        hex += &f;
-    }
- 
+    let hex = text_to_hex(&buffer);
+    
     // output hex string
     println!("in hexedecimal is\n{hex}");
 }
